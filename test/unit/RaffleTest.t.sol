7ede80e4ce4ce447991d0b980fc1edd6cbe82d30
Francisco Cordeiro <francisco.cordeiro.business@gmail.com>
2025-06-20 18:06:42 +0100

    Unit tests setup

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {Raffle} from "src/Raffle.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+contract RaffleTest is Test {
+    Raffle private raffle;
+    HelperConfig private helperConfig;
+
+    address private PLAYER = makeAddr("PLAYER");
+    uint256 private PLAYER_STARTING_BALANCE = 10 ether;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint64 subscriptionId;
+    uint32 callbackGasLimit;
+    uint256 entranceFee;
+    uint256 interval;
+
+    function setUp() external {
+        DeployRaffle deployRaffle = new DeployRaffle();
+        (raffle, helperConfig) = deployRaffle.deployContract();
+
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        vrfCoordinator = config.vrfCoordinator;
+        gasLane = config.gasLane;
+        subscriptionId = config.subscriptionId;
+        callbackGasLimit = config.callbackGasLimit;
+        entranceFee = config.entranceFee;
+        interval = config.interval;
+
+        vm.deal(PLAYER, PLAYER_STARTING_BALANCE);
+    }
+
+    function testRaffleInitializesInOpenState() external view {
+        assert(Raffle.RaffleState.OPEN == raffle.getRaffleState());
+    }
+}
